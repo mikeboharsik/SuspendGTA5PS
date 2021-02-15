1d4e6d60640d21373098baa52684e02c0e570406
ganonscrub <mboharsik@gmail.com>
2021-02-15 13:17:30 -0500

    Add documentation to root script

--- Invoke-SuspendGTA5.ps1
+++ Invoke-SuspendGTA5.ps1
@@ -1,5 +1,31 @@
 # this has been adapted from https://web.archive.org/web/20201111195553/https://gist.github.com/19WAS85/5424431
 
+<#
+.SYNOPSIS
+  Facilitates the suspension and unsuspension of the GTA5.exe process
+
+.DESCRIPTION
+  Runs an HTTP listener that allows Win32 API functions to be invoked
+  either by the user running on localhost or optionally via another
+  device on the network, e.g. a phone.
+
+.LINK
+ https://github.com/mikeboharsik/SuspendGTA5PS
+
+.PARAMETER Port
+  Specifies the port on which to listen for HTTP requests, defaults
+  to 8080
+
+.PARAMETER LocalOnly
+  If set, skips attempting to bind to network-accessible IP addresses
+  and hostnames
+
+.EXAMPLE
+  .\Invoke-SuspendGTA5.ps1
+  Default behavior that runs on localhost:8080 and tries to bind
+  to hostnames if running in an administrator context
+#>
+
 [CmdletBinding()]
 Param(
   [int] $Port = 8080,
